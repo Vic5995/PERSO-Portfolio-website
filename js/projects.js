fa7245130a6608e591912ff7320928cedf022890
Victoire <victoire.lenglart.5995@gmail.com>
2022-02-25 18:27:38 +0100

    Updates

---
+++ js/projects.js
@@ -0,0 +1,58 @@
+export const projects = [
+    {
+        "category": "ANDROID PROJECTS",
+        "items": [
+            {
+                "imgHref": "#",
+                "imgSrc": "res/img/projects/project-5-android-trivia/project-5-trivia-app-mini.png",
+                "projectPage": "project-pages/project5-android-trivia.html",
+                "gitUrl": "https://github.com/Vic5995/PERSO-Android-trivia-app"
+            },
+            {
+                "imgHref": "#",
+                "imgSrc": "res/img/projects/project-6-android-todo/project-6-todo-app-mini.png",
+                "projectPage": "project-pages/project6-android-todo.html",
+                "gitUrl": "https://github.com/Vic5995/PERSO-Android-todo-app"
+            }
+        ]
+    },
+    {
+        "category": "WEB PROJECTS",
+        "items": [
+            {
+                "imgHref": "https://vic5995.github.io/PERSO-countdown-timer/",
+                "imgSrc": "res/img/projects/project-3-countdown-timer/project-3-main-page.png",
+                "projectPage": "project-pages/project3-countdown-timer.html",
+                "gitUrl": "https://github.com/Vic5995/PERSO-countdown-timer"
+            },
+            {
+                "imgHref": "https://vic5995.github.io/PERSO-alpaca-image-generator-website/",
+                "imgSrc": "res/img/projects/project-4-alpaca-generator/project-4-main.png",
+                "projectPage": "project-pages/project4-alpaca-generator.html",
+                "gitUrl": "https://github.com/Vic5995/PERSO-alpaca-image-generator-website"
+            }
+        ]
+    },
+    {
+        "category": "SCHOOL PROJECTS",
+        "items": [
+            {
+                "imgHref": "project-pages/project1-covid-dashboard.html",
+                "imgSrc": "res/img/projects/project-1-covid-dashboard/project1.png",
+                "projectPage": "project-pages/project1-covid-dashboard.html",
+                "gitUrl": "https://github.com/Vic5995/IMT_A1_ACDC_Dashboard"
+            }
+        ]
+    },
+    {
+        "category": "DATA PROJECTS",
+        "items": [
+            {
+                "imgHref": "project-pages/project2-data-analisys-DGFIP.html",
+                "imgSrc": "res/img/projects/project-2-online-data-courses/project2.png",
+                "projectPage": "project-pages/project2-data-analisys-DGFIP.html",
+                "gitUrl": "https://github.com/Vic5995/PERSO-Data/blob/main/EN/EDA-online-data-courses-EN.ipynb"
+            }
+        ]
+    },
+]
